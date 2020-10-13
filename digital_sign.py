baefa79821ea19396465a81d5d02ba2166507f32
Jason Brown <jason@jasonbrown.us>
2020-10-13 16:02:49 -0400

    Initial commit

--- digital_sign.py
+++ digital_sign.py
@@ -8,26 +8,10 @@ __date__   = '20201013'
 
 '''Import those modules!'''
 from OpenSSL import crypto
-from botocore.exceptions import ClientError
 from os import system
-import boto3, json, base64
+import json, base64
 
 def main():
-
-    '''Connection to AWS Secrets Manager'''
-    secret_name = 'prod/causer_ca/n3fips'
-    region_name = 'us-east-2'
-
-    client = boto3.client(
-        service_name='secretsmanager',
-        region_name=region_name
-    )
-
-    response = client.get_secret_value(SecretId=secret_name)
-    secret = json.loads(response['SecretString'])
-
-    '''Retrieve username:password from Secrets Manager and export it per CloudHSM requirements'''
-    system('export n3fips_password=' + secret['username'] + ':' +secret['password'])
 
     privkey_file = open('privatekey.pem', 'r')
     privkey = privkey_file.read()
@@ -36,11 +20,11 @@ def main():
 
     '''And this is where the magic happens'''
     '''Grab the file and sign it'''
-    hashed = open('winners.csv', 'rb').read()
+    hashed = open('', 'rb').read()
     signed = crypto.sign(pkey, hashed, 'sha256')
     signedb64 = base64.encodebytes(signed).decode()
 
-    signature = open('signature.sig', 'w')
+    signature = open('', 'w')
     signature.write(signedb64)
     signature.close()
 
